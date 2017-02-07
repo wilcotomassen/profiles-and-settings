037acbce9c246dcc13601edc5160c782b88b20f0
Wilco Tomassen <wilco@wilcotomassen.nl>
2017-02-07 19:34:23 +0100

    Added bash/ssh-agent.sh

---
+++ bash/ssh-agent.sh
@@ -0,0 +1,12 @@
+# SSH agent for remembering SSH key passphrase
+SSH_ENV=$HOME/.ssh/environment
+function start_agent {
+    echo "Initializing new SSH agent..."
+    # spawn ssh-agent
+    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+    echo succeeded
+    chmod 600 "${SSH_ENV}"
+    . "${SSH_ENV}" > /dev/null
+    /usr/bin/ssh-add
+}
+alias ssh-start='start_agent'
